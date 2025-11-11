b87473e453b156f600f102872ac9355b8a07cb41
snadi043 <snadi043@uottawa.ca>
2025-11-10 19:09:40 -0500

    Initial commit to setup the project initializations, configurations and installations.

---
+++ app.js
@@ -0,0 +1,9 @@
+// Importing the Express Framework into the project.
+// Before importing install the package using "npm install --save express" command from your terminal
+const express = require('express');
+
+// Configuring the application to use express.
+const app = express();
+
+// Configuring the application to listen to the port 3000 on the browser.
+app.listen(3000);
