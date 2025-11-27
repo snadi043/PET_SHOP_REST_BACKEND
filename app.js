67166190863437c5a636419a67e90602836a67d5
snadi043 <snadi043@uottawa.ca>
2025-11-27 07:57:05 -0500

    Installing the mongodb package to implement the integration of node application with "NOSQL" database concepts with "MONOGODB".

--- app.js
+++ app.js
@@ -9,18 +9,21 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 // Importing the db utility module to create the connection with the database.
-const sequelize = require('./utils/database');
+// const sequelize = require('./utils/database');
+
+// Importing the MongoClient from the utility directory to create the connection to the "MONGODB" backend database.
+const mongoConnect = require('./utils/database').mongoConnect;
 
 // Importing the error controller
 const errorController = require('./controllers/error');
 
 //Importing the models to build the association relationship between them in conjucion with the database interactions.
-const Product = require('./models/product');
-const Cart = require('./models/cart');
-const CartItems = require('./models/cart-item');
-const User = require('./models/user');
-const Orders = require('./models/orders');
-const OrderItems = require('./models/order-item');
+// const Product = require('./models/product');
+// const Cart = require('./models/cart');
+// const CartItems = require('./models/cart-item');
+// const User = require('./models/user');
+// const Orders = require('./models/orders');
+// const OrderItems = require('./models/order-item');
 
 // Importing all the routes in the application to register in the app.js file so that routing happens in an organized manner. 
 const adminProductRoutes = require('./routes/admin/admin-products');
@@ -47,12 +50,12 @@ app.use(bodyParser.urlencoded({extended: false}));
 // });
 
 // Creating a middleware to manually log user into the application by injecting the user onto the request.
-app.use((req, res, next) => {
-    User.findByPk(1).then(user => {
-        req.user = user; // This is the format using which you can add fields to the request "apart from already registered keywords in the request".
-        next();
-    }).catch(err => {console.log(err)});
-});
+// app.use((req, res, next) => {
+//     User.findByPk(1).then(user => {
+//         req.user = user; // This is the format using which you can add fields to the request "apart from already registered keywords in the request".
+//         next();
+//     }).catch(err => {console.log(err)});
+// });
 
 // Congiruing all the routes to be registered with the express framework.
 app.use('/admin', adminProductRoutes);
@@ -63,33 +66,38 @@ app.use(errorController.getErrorPage);
 // Associations are the one of the important concepts in SEQUELIZE library.
 
 // lines 55 & 56 creates an association between User and Product Models.
-Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'}); 
-User.hasMany(Product);
+// Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'}); 
+// User.hasMany(Product);
 
-// Lines 66 & 67 creates an association between User, Product and Cart Models.
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Product.belongsToMany(Cart, {through: CartItems});
-Cart.belongsToMany(Product, {through: CartItems});
-Orders.belongsTo(User);
-User.hasMany(Orders);
-Orders.belongsToMany(Product, {through: OrderItems});
-Product.belongsToMany(Orders, {through: OrderItems});
+// // Lines 66 & 67 creates an association between User, Product and Cart Models.
+// User.hasOne(Cart);
+// Cart.belongsTo(User);
+// Product.belongsToMany(Cart, {through: CartItems});
+// Cart.belongsToMany(Product, {through: CartItems});
+// Orders.belongsTo(User);
+// User.hasMany(Orders);
+// Orders.belongsToMany(Product, {through: OrderItems});
+// Product.belongsToMany(Orders, {through: OrderItems});
 
 // Importing the database connection module here to connect it with the application.
-sequelize.sync().then((result) => {
-    return User.findByPk(1);
-}).then((user) => {
-    if(!user){
-        User.create({name: 'SAI', email: 'test@test.com'});
-    }
-    return user;
-}).then(user => {
-    return user.createCart();
-}).then(cart => {
-    app.listen(3000);
-})
-.catch(err => {console.log(err)});
+// sequelize.sync().then((result) => {
+//     return User.findByPk(1);
+// }).then((user) => {
+//     if(!user){
+//         User.create({name: 'SAI', email: 'test@test.com'});
+//     }
+//     return user;
+// }).then(user => {
+//     return user.createCart();
+// }).then(cart => {
+//     app.listen(3000);
+// })
+// .catch(err => {console.log(err)});
 
 // Using the Sequelize exports 
 // Configuring the application to listen to the port 3000 on the browser.
+
+
+mongoConnect(() => {
+    app.listen(3000);
+});
